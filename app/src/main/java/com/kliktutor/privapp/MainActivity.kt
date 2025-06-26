8463dd1d8f3069cb19fd7bd07ac13080f42590ca
Sandy565 <sandypratama987@gmail.com>
2025-06-26 15:05:49 +0700

    penggantian warna tombol navigasi

--- app/src/main/java/com/kliktutor/privapp/MainActivity.kt
+++ app/src/main/java/com/kliktutor/privapp/MainActivity.kt
@@ -4,12 +4,18 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var contentTextView: TextView
+    private lateinit var bottomNavigationView: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -19,9 +25,9 @@ class MainActivity : AppCompatActivity() {
 //        1. var (mutable)
 //        2. val (imutable)
 //        deklarasi variabek object
-        val btn1 = findViewById<Button>(R.id.btn_1)
-        val btn2 : Button = findViewById(R.id.btn_2)
-        val btn3 : Button = findViewById(R.id.btn_3)
+        val btn1 = findViewById<Button>(R.id.btn_2)
+        val btn2 : Button = findViewById(R.id.btn_3)
+        val btn3 : Button = findViewById(R.id.btn_4)
 
 //        buat event
 //        1. event btn1
