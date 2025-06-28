eb8e5c8933d6f592071574f1589b4e101f9a9eb1
Sandy565 <sandypratama987@gmail.com>
2025-06-28 23:54:11 +0700

    penambahan halaman login

--- app/src/main/java/com/kliktutor/privapp/MainActivity.kt
+++ app/src/main/java/com/kliktutor/privapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.kliktutor.privapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
@@ -19,7 +20,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+//        setContentView(R.layout.activity_main)
+
+        val prefs = getSharedPreferences("PrivAppPrefs", MODE_PRIVATE)
+        val isLoggedIn = prefs.getBoolean("isLoggedIn", false)
+
+        if (!isLoggedIn) {
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+            return
+        }
+
         setContentView(R.layout.activity_main)
+
 
         btnBeranda = findViewById(R.id.btn_1)
         btnJadwal = findViewById(R.id.btn_2)
@@ -42,9 +55,10 @@ class MainActivity : AppCompatActivity() {
             loadFragment(akunfragment())
         }
 
-        // Tampilkan Beranda secara default
+        // Tampilkan fragment awal (beranda)
         loadFragment(berandafragment())
 
+        // Insets agar tidak tabrakan sama status bar/navigation
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -52,7 +66,6 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    // FUNGSI INI HARUS DI LUAR onCreate()
     private fun loadFragment(fragment: Fragment) {
         supportFragmentManager.beginTransaction()
             .replace(R.id.fragment_container, fragment)
